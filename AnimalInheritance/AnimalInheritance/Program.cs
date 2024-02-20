0805b7b1c5a8b61f91b720721ce3f276f145b0d7
Eurykles <mrcaysizzle@gmail.com>
2024-02-20 11:08:02 -0500

    added AnimalInheritance

---
+++ AnimalInheritance/AnimalInheritance/Program.cs
@@ -0,0 +1,86 @@
+﻿namespace AnimalInheritance
+{
+    // base Class 
+    class Animal
+    {
+        private string name; // only accessible within this class
+        protected string type; // accessible from derived classes
+        public string color;  // accessible from any class
+
+        public void setName(string name)
+        {
+            this.name = name;
+        }
+        public virtual string getName()
+        {
+            return this.name;
+        }
+        public void setType(string type)
+        {
+            this.type = type;
+        }
+        public virtual string getType()
+        {
+            return this.type;
+        }
+    }
+    class Elephant : Animal
+    {
+        public int weight;
+        public int height;
+        private string temperament;
+        public void setTemperament(string temperament)
+        {
+            this.temperament = temperament;
+        }
+        public virtual string getTemperament()
+        {
+            return this.temperament;
+        }
+        public override string getName()
+        {
+            return base.getName();
+        }
+        public override string getType()
+        {
+            return base.getType();
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // base class object
+            Animal animal = new Animal();
+            animal.setName("Dash");
+            animal.setType("Grayhound");
+            // color can be accessed anywhere - it's public
+            animal.color = "gray";
+
+            Console.WriteLine($">> Displaying animal information");
+            Console.WriteLine($"Name: {animal.getName()}");
+            Console.WriteLine($"Type: {animal.getType()}");
+            Console.WriteLine($"Color: {animal.color}");
+            Console.WriteLine();
+
+            // object of derived class Elephant
+            Elephant elephant = new Elephant();
+            elephant.setName("Dumbo");
+            elephant.setType("african");
+            // color can be accessed anywhere - it's public
+            elephant.color = "gray";
+            elephant.weight = 13000;
+            elephant.height = 12;
+            // temperament is protected - it can't be altered publicly like color
+            elephant.setTemperament("bullish");
+
+            Console.WriteLine(">> Displaying elephant information");
+            Console.WriteLine($"Name: {elephant.getName()}");
+            Console.WriteLine($"Type: {elephant.getType()}");
+            Console.WriteLine($"Color: {elephant.color}");
+            Console.WriteLine($"Weight: {elephant.weight} lbs.");
+            Console.WriteLine($"Height: {elephant.height} ft.");
+            Console.WriteLine($"Temperament: {elephant.getTemperament()}");
+        }
+    }
+}
--- SimpleInheritance/SimpleInheritance/obj/Debug/net8.0/SimpleInheritance.AssemblyInfo.cs
+++ SimpleInheritance/SimpleInheritance/obj/Debug/net8.0/SimpleInheritance.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SimpleInheritance")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+54686ff833a05ca6fc8cec3ac14049bcb91116e7")]
 [assembly: System.Reflection.AssemblyProductAttribute("SimpleInheritance")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SimpleInheritance")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
