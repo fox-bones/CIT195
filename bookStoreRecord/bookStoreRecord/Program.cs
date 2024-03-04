f5776af01df70ac8946733d15da9e0b67a4ccfdc
Eurykles <mrcaysizzle@gmail.com>
2024-03-04 16:04:27 -0500

    added immuteableID, updated bookStoreRedord

--- bookStoreRecord/bookStoreRecord/Program.cs
+++ bookStoreRecord/bookStoreRecord/Program.cs
@@ -2,43 +2,25 @@
 {
     internal class Program
     {
-        public record Bookstore
-        {
-            public int Id { get; init; }
-            public string Title { get; init; }
-            public string Author { get; init; }
-            public double Price { get; init; }
-            public Bookstore()
-            {
-                Id = 0;
-                Title = string.Empty;
-                Author = string.Empty;
-                Price = 0;
-            }
-            public Bookstore(int id, string title, string author, double price)
-            {
-                Id = id;
-                Title = title;
-                Author = author;
-                Price = price;
-            }
-            public void Display()
-            {
-                Console.WriteLine(Id);
-                Console.WriteLine(Title);
-                Console.WriteLine(Author);
-                Console.WriteLine("$" + Price);
-            }
-        }
+        public record Bookstore(int Id, string Title, string Author, double Price);
         static void Main(string[] args)
         {
             Bookstore AGT = new Bookstore(1, "A Game of Thrones", "George R. R. Martin", 12.99);
             Bookstore TGWDT = new Bookstore(2, "The Girl with the Dragon Tattoo", "Stieg Larson", 14.99);
             Bookstore LITM = new Bookstore(3, "Latro in the Mist", "Gene Wolfe", 13.99);
 
-            AGT.Display();
-            TGWDT.Display();
-            LITM.Display();
+            Console.WriteLine("ID: " + AGT.Id);
+            Console.WriteLine("Title: " + AGT.Title);
+            Console.WriteLine("Author: " + AGT.Author);
+            Console.WriteLine("Price: " + AGT.Price);
+            Console.WriteLine("ID: " + TGWDT.Id);
+            Console.WriteLine("Title: " + TGWDT.Title);
+            Console.WriteLine("Author: " + TGWDT.Author);
+            Console.WriteLine("Price: " + TGWDT.Price);
+            Console.WriteLine("ID: " + LITM.Id);
+            Console.WriteLine("Title: " + LITM.Title);
+            Console.WriteLine("Author: " + LITM.Author);
+            Console.WriteLine("Price: " + LITM.Price);
         }
     }
 }
---
+++ immutableID/immutableID/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace immutableID
+{
+    internal class Program
+    {
+        class Student
+        {
+            // auto-implemented properties
+            public int Id { get; set; }
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+
+            // default constructor
+            public Student()
+            {
+                Id = 0;
+                FirstName = "";
+                LastName = "";
+            }
+            // parameterized constructor
+            public Student(int i, string First, string Last)
+            {
+                Id = i;
+                FirstName = First;
+                LastName = Last;
+            }
+            public Student(int i)
+            {
+                Id = i;
+                FirstName = "";
+                LastName = "";
+            }
+
+        }
+        static void Main(string[] args)
+        {
+            Student a0001 = new Student(1);
+            a0001.FirstName = "Gretchen";
+            a0001.LastName = "Briar";
+            Student a0002 = new Student(2, "Richard", "Martin");
+            
+            Console.WriteLine(a0001.Id);
+            Console.WriteLine(a0001.FirstName);
+            Console.WriteLine(a0001.LastName);
+            Console.WriteLine();
+            Console.WriteLine(a0002.Id);
+            Console.WriteLine(a0002.FirstName);
+            Console.WriteLine(a0002.LastName);
+        }
+    }
+}
