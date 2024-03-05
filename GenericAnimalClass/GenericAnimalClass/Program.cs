d6852f4736201059a97c94cb6a84fcaa41e7141f
Eurykles <mrcaysizzle@gmail.com>
2024-03-05 11:25:40 -0500

    added GenericAnimalClass

---
+++ GenericAnimalClass/GenericAnimalClass/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace GenericAnimalClass
+{
+    public class Animal <T>
+    {
+        public T data;
+        public Animal(T data)
+        {
+            this.data = data;
+        }
+        public T getAnimal()
+        {
+            return data;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // setting animal properties
+            Animal<string> animalName = new Animal<string>("Toucan");
+            Animal<string> animalHabitat = new Animal<string>("Canopies of Central and South American rain forests");
+            Animal<bool> endangered = new Animal<bool>(false);
+            Animal<bool> extinct = new Animal<bool>(false);
+            Animal<int> averageLifespan = new Animal<int>(16);
+            // display animal information
+            Console.WriteLine("Name: " + animalName.getAnimal());
+            Console.WriteLine("Habitat: " + animalHabitat.getAnimal());
+            Console.WriteLine("Endangered: " + endangered.getAnimal());
+            Console.WriteLine("Extinct: " + extinct.getAnimal());
+            Console.WriteLine("Average Lifespan: " + averageLifespan.getAnimal() + " years");
+        }
+    }
+}
